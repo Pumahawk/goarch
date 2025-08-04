eff0dd0444eab148ff0987fb76034297ded6d97d
Lorenzo Gandino <lorenzoga94@gmail.com>
2025-08-04 22:09:24 +0200

    Create ls and run

--- cmd/main.go
+++ cmd/main.go
@@ -3,22 +3,101 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
+
+	yaml "github.com/goccy/go-yaml"
 )
 
 type Service struct {
-	Name string   `yaml:"name"`
-	Exec string   `yaml:"exec"`
-	Args []string `yaml:"args"`
+	Name  string   `yaml:"name"`
+	Exec  string   `yaml:"exec"`
+	Args  []string `yaml:"args"`
+	StdIn *string  `yaml:"stdIn"`
+}
+
+func RunService(service *Service) {
+	cmd := exec.Command(service.Exec, service.Args...)
+	var stdin io.Reader
+	if service.StdIn == nil {
+		stdin = os.Stdin
+	} else {
+		stdin = strings.NewReader(*service.StdIn)
+	}
+	cmd.Stdin = stdin
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
 
 type Conf struct {
-	Services string `yaml:"services"`
+	Services []Service `yaml:"services"`
+}
+
+type LsCommandFlags struct {
 }
 
 type MainFlags struct {
 	ConfPath string
+}
+
+func RunCommand(mf *MainFlags, args []string) {
+	_, args, err := LoadRunCommandFlags(args)
+	if err != nil {
+		log.Fatalf("[run] Unable to parse flags. %s", err)
+	}
+	conf, err := LoadConf(mf.ConfPath)
+	if err != nil {
+		log.Fatalf("[run] Unable to load conf file. %s", err)
+	}
+	if len(args) < 1 {
+		log.Fatalf("[run] missin service name")
+	}
+	serviceNameRun := args[0]
+	for _, s := range conf.Services {
+		if s.Name == serviceNameRun {
+			RunService(&s)
+			return
+		}
+	}
+	log.Fatalf("Service %s not found", serviceNameRun)
+}
+
+func LsCommand(mf *MainFlags, args []string) {
+	_, args, err := LoadLsCommandFlags(args)
+	if err != nil {
+		log.Fatalf("[ls] Unable to parse flags. %s", err)
+	}
+	conf, err := LoadConf(mf.ConfPath)
+	if err != nil {
+		log.Fatalf("[ls] Unable to load conf file. %s", err)
+	}
+	for _, s := range conf.Services {
+		fmt.Printf("%s\n", s.Name)
+	}
+}
+
+func LoadLsCommandFlags(args []string) (*LsCommandFlags, []string, error) {
+	var lsCommandFlags LsCommandFlags
+	fls := flag.NewFlagSet("", flag.ExitOnError)
+	err := fls.Parse(args)
+	if err != nil {
+		return nil, nil, fmt.Errorf("[LoadLsCommandFlags] Unable to parse falgs. %w", err)
+	}
+	return &lsCommandFlags, fls.Args(), nil
+}
+
+func LoadRunCommandFlags(args []string) (*LsCommandFlags, []string, error) {
+	var lsCommandFlags LsCommandFlags
+	fls := flag.NewFlagSet("", flag.ExitOnError)
+	err := fls.Parse(args)
+	if err != nil {
+		return nil, nil, fmt.Errorf("[LoadRunCommandFlags] Unable to parse falgs. %w", err)
+	}
+	return &lsCommandFlags, fls.Args(), nil
 }
 
 func LoadMainFlags(args []string) (*MainFlags, []string, error) {
@@ -32,18 +111,30 @@ func LoadMainFlags(args []string) (*MainFlags, []string, error) {
 	return &mainFlags, fls.Args(), nil
 }
 
+func LoadConf(path string) (*Conf, error) {
+	var conf Conf
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("[load-conf] Unable to load file %s. %w", path, err)
+	}
+	err = yaml.Unmarshal(content, &conf)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse file %s. %w", path, err)
+	}
+	return &conf, nil
+}
+
 func main() {
-	nfls, args, err := LoadMainFlags(os.Args[1:])
-	log.Printf("Conf path: %s", nfls.ConfPath)
+	conf, args, err := LoadMainFlags(os.Args[1:])
 	if err != nil {
 		log.Fatalf("[main] Unable to parse flags. %s", err)
 	}
-	if len(args) > 1 {
-		switch args[1] {
+	if len(args) > 0 {
+		switch args[0] {
 		case "ls":
-			log.Println("ls command")
+			LsCommand(conf, args[1:])
 		case "run":
-			log.Println("run command")
+			RunCommand(conf, args[1:])
 		}
 	} else {
 		log.Fatal("Missing command parameter")
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pumahawk/gorch
 
 go 1.23.1
+
+require github.com/goccy/go-yaml v1.18.0 // indirect
